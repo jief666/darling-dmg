54f9c2e785c8f899ae18c3c40ae1e4e86838bf4a
jief <jief@x220desktop-2.home>
2018-09-02 13:44:05 +0200

    CacheZone object are now shared_ptr.
    Some CacheZone object were shared, but could be destroyed.
    For example, once you called DMGDisk::readerForPartition, you can now
    delete DMGDisk and only keep the Reader object.
    
    Took the occasion to remove unused mustbreak integer.

--- src/DMGDisk.cpp
+++ src/DMGDisk.cpp
@@ -29,7 +29,7 @@ static uint64_t findKolyBlock(std::shared_ptr<Reader> reader)
 }
 
 DMGDisk::DMGDisk(std::shared_ptr<Reader> reader)
-	: m_reader(reader), m_zone(40000)
+	: m_reader(reader), m_zone(std::make_shared<CacheZone>(40000))
 {
 	uint64_t offset = findKolyBlock(m_reader);
 
@@ -253,11 +253,11 @@ std::shared_ptr<Reader> DMGDisk::readerForPartition(int index)
 					m_reader->length() - data_offset));
 
 				return std::shared_ptr<Reader>(
-						new CachedReader(std::shared_ptr<Reader>(new DMGPartition(r, table)), &m_zone, partName.str())
+						new CachedReader(std::shared_ptr<Reader>(new DMGPartition(r, table)), m_zone, partName.str())
 						);
 			} else {
 				return std::shared_ptr<Reader>(
-						new CachedReader(std::shared_ptr<Reader>(new DMGPartition(m_reader, table)), &m_zone, partName.str())
+						new CachedReader(std::shared_ptr<Reader>(new DMGPartition(m_reader, table)), m_zone, partName.str())
 						);
 			}
 		}
